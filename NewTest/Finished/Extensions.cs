d0a13ca4356c8cc23b3ac4b703dd99a646884cc6
Виталий Зуев <zuevitalij@yandex.ru>
2019-08-13 11:01:53 +0200

    дополнено расширение Equal

--- NewTest/Finished/Extensions.cs
+++ NewTest/Finished/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,19 @@ namespace NewTest
         {
             Type typeA = typeof(TypeA);
             Type typeB = typeof(TypeB);
-            var fieldsA = typeA.GetFields();
-            var fieldsB = typeB.GetFields();
-            return (from a in fieldsA
-                    from b in fieldsB
-                    where a.Name == b.Name
-                    select a.GetValue(A).Equals(b.GetValue(B))).All(x => x == true);
+            FieldInfo[] fieldsA = typeA.GetFields();
+            FieldInfo[] fieldsB = typeB.GetFields();
+            bool equalFields = (from a in fieldsA
+                                from b in fieldsB
+                                where a.Name == b.Name
+                                select a.GetValue(A).Equals(b.GetValue(B))).All(x => x == true);
+            PropertyInfo[] propertiesA = typeA.GetProperties();
+            PropertyInfo[] propertiesB = typeB.GetProperties();
+            bool equalProperties = (from a in propertiesA
+                                    from b in propertiesB
+                                    where a.Name == b.Name
+                                    select a.GetValue(A).Equals(b.GetValue(B))).All(x => x == true);
+            return equalFields && equalProperties;
         }
     }
 }
