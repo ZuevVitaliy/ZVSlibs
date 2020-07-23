f9d83f7a3a7c204a5645881a1700f5dfcdddba0a
Виталий Зуев <zuevitalij@yandex.ru>
2020-07-23 17:09:15 +0200

    Add libs project for WPF. Override Prism.DelegateCommand class

---
+++ ZVS.WPF.Libs/Models/Commands/DelegateCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ZVS.WPF.Libs.Models.Commands
+{
+    public class DelegateCommand : Prism.Commands.DelegateCommand
+    {
+        public DelegateCommand(Action executeMethod) : base(executeMethod)
+        {
+        }
+
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod) : base(executeMethod, canExecuteMethod)
+        {
+        }
+
+        public override event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                base.CanExecuteChanged += value;
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                base.CanExecuteChanged -= value;
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+
+    public class DelegateCommand<T> : Prism.Commands.DelegateCommand<T>
+    {
+        public DelegateCommand(Action<T> executeMethod) : base(executeMethod)
+        {
+        }
+
+        public DelegateCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod) : base(executeMethod, canExecuteMethod)
+        {
+        }
+
+        public override event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                base.CanExecuteChanged += value;
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                base.CanExecuteChanged -= value;
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
