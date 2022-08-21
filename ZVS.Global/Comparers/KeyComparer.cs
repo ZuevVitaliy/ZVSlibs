9ec4a3c7e6b1432b2dab1069e250117f799ec60d
zuevitalij <zuevitalij@yandex.ru>
2022-08-21 04:29:58 +0200

    Улучшение LINQ

---
+++ ZVS.Global/Comparers/KeyComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZVS.Global.Comparers
+{
+    /// <summary>
+    /// Компаратор на основе ключей.
+    /// </summary>
+    public class KeyComparer<TElement, TKey> : IEqualityComparer<TElement>
+    {
+        private readonly Func<TElement, TKey> _keyProvider;
+        
+        public KeyComparer(Func<TElement, TKey> keyProvider)
+        {
+            _keyProvider = keyProvider;
+        }
+
+        public bool Equals(TElement first, TElement second)
+        {
+            var firstProp = _keyProvider(first);
+            var secondProp = _keyProvider(second);
+
+            return EqualityComparer<TKey>.Default.Equals(firstProp, secondProp);
+        }
+
+        public int GetHashCode(TElement obj)
+        {
+            return _keyProvider(obj).GetHashCode();
+        }
+    }
+}
--- ZVS.Global/Extensions/EnumerableExtensions.cs
+++ ZVS.Global/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using ZVS.Global.Comparers;
 
 namespace ZVS.Global.Extensions
 {
@@ -19,6 +21,22 @@ namespace ZVS.Global.Extensions
             IEnumerator enumerator = source.GetEnumerator();
             enumerator.Reset();
             return !enumerator.MoveNext();
+        }
+
+        /// <summary>
+        /// Выборка уникальных значений, по ключу.
+        /// </summary>
+        /// <typeparam name="TSource">Тип перечисляемых элементов.</typeparam>
+        /// <typeparam name="TKey">Тип ключа.</typeparam>
+        /// <param name="source">Исходное перечисляемое.</param>
+        /// <param name="keySelector">Ключ для выборки уникальных значений из перечисляемого</param>
+        /// <remarks><i><b>Примечание:</b> Используется отложенный итератор <see langword="yield"/> для перечисляемого.<br/>
+        /// Для полной выборки используйте методы расширения ToList() или ToArray().<br/>
+        /// Если необходим составной ключ, то воспользуйтесь анонимным типом (new { key1, key2 })</i><br/></remarks>
+        /// <returns>Уникальные значения по ключу.</returns>
+        public static IEnumerable<TSource> Distinct<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            return source.Distinct(new KeyComparer<TSource, TKey>(keySelector));
         }
 
         /// <summary>
@@ -78,5 +96,91 @@ namespace ZVS.Global.Extensions
                     break;
             }
         }
+
+
+        /// <summary>
+        /// Проверка на совпадение элементов двух перечисляемых, игнорируя порядок этих элементов и наличие дублей.
+        /// <br><i>Для сравнения элементов используется <see cref="HashSet{T}"/>.</i></br>
+        /// </summary>
+        /// <param name="thisEnumerable">Текущее перечисляемое.</param>
+        /// <param name="otherEnumerable">Сравниваемое перечисляемое.</param>
+        /// <returns>Если перечисляемые содержат одинаковые ключевые элементы, то <see langword="true"/>, иначе <see langword="false"/>.</returns>
+        public static bool SequenceEqualIgnoreOrderAndDuplicates<TValue>(this IEnumerable<TValue> thisEnumerable, IEnumerable<TValue> otherEnumerable)
+        {
+            return thisEnumerable.SequenceEqualIgnoreOrderAndDuplicates<object, TValue>(otherEnumerable, null);
+        }
+
+        /// <summary>
+        /// Проверка на совпадение элементов двух перечисляемых, игнорируя порядок этих элементов и наличие дублей.
+        /// <br><i>Для сравнения элементов используется <see cref="HashSet{T}"/>.</i></br>
+        /// </summary>
+        /// <param name="thisEnumerable">Текущее перечисляемое.</param>
+        /// <param name="otherEnumerable">Сравниваемое перечисляемое.</param>
+        /// <param name="keySelector">Функция выборки ключа.</param>
+        /// <returns>Если перечисляемые содержат одинаковые ключевые элементы, то <see langword="true"/>, иначе <see langword="false"/>.</returns>
+        public static bool SequenceEqualIgnoreOrderAndDuplicates<TKey, TValue>(this IEnumerable<TValue> thisEnumerable, IEnumerable<TValue> otherEnumerable, Func<TValue, TKey> keySelector)
+        {
+            if (thisEnumerable == null && otherEnumerable == null)
+                return true;
+            if (thisEnumerable == null || otherEnumerable == null)
+                return false;
+
+            if (keySelector == null)
+            {
+                var thisSet = thisEnumerable.ToHashSet();
+                var otherSet = otherEnumerable.ToHashSet();
+
+                return thisSet.SetEquals(otherSet);
+            }
+            else
+            {
+                var thisSet = thisEnumerable.Select(keySelector).ToHashSet();
+                var otherSet = otherEnumerable.Select(keySelector).ToHashSet();
+
+                return thisSet.SetEquals(otherSet);
+            }
+        }
+
+        /// <summary>
+        /// Проверка на совпадение элементов двух перечисляемых, игнорируя порядок этих элементов.
+        /// </summary>
+        /// <param name="thisEnumerable">Текущее перечисляемое.</param>
+        /// <param name="otherEnumerable">Сравниваемое перечисляемое.</param>
+        /// <returns>Если перечисляемые содержат одинаковые ключевые элементы, то <see langword="true"/>, иначе <see langword="false"/>.</returns>
+        public static bool SequenceEqualIgnoreOrder<TValue>(this IEnumerable<TValue> thisEnumerable, IEnumerable<TValue> otherEnumerable)
+        {
+            return thisEnumerable.SequenceEqualIgnoreOrder<object, TValue>(otherEnumerable, null);
+        }
+
+        /// <summary>
+        /// Проверка на совпадение элементов двух перечисляемых, игнорируя порядок этих элементов.
+        /// </summary>
+        /// <param name="thisEnumerable">Текущее перечисляемое.</param>
+        /// <param name="otherEnumerable">Сравниваемое перечисляемое.</param>
+        /// <param name="keySelector">Функция выборки ключа.</param>
+        /// <returns>Если перечисляемые содержат одинаковые ключевые элементы, то <see langword="true"/>, иначе <see langword="false"/>.</returns>
+        public static bool SequenceEqualIgnoreOrder<TKey, TValue>(this IEnumerable<TValue> thisEnumerable, IEnumerable<TValue> otherEnumerable, Func<TValue, TKey> keySelector)
+        {
+            if (thisEnumerable == null && otherEnumerable == null)
+                return true;
+            if (thisEnumerable == null || otherEnumerable == null)
+                return false;
+
+            if (keySelector == null)
+            {
+                var thisOrderedEnumerable = thisEnumerable.OrderBy(x => x.GetHashCode());
+                var otherOrderedEnumerable = otherEnumerable.OrderBy(x => x.GetHashCode());
+
+                return thisOrderedEnumerable.SequenceEqual(otherOrderedEnumerable);
+            }
+            else
+            {
+                var thisOrderedEnumerable = thisEnumerable.Select(keySelector).OrderBy(x => x.GetHashCode());
+                var otherOrderedEnumerable = otherEnumerable.Select(keySelector).OrderBy(x => x.GetHashCode());
+
+                return thisOrderedEnumerable.SequenceEqual(otherOrderedEnumerable);
+            }
+        }
+
     }
 }
